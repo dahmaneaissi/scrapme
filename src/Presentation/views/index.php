7647948fb472c65b10cd833feb00f9156c13b54e
Dahmane Aissi <aissi.dahmane@gmail.com>
2022-07-17 20:22:53 +0100

    fix code standard

--- src/Presentation/views/index.php
+++
@@ -1,3 +0,0 @@
-<p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dicta, distinctio incidunt laborum neque suscipit. Consequatur, doloribus expedita facere molestiae necessitatibus omnis pariatur provident repellat temporibus totam velit, voluptas voluptates.
-</p>
